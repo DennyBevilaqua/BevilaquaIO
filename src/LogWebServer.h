6c503ad588f7790646dd7a6d5d2f2da478070729
denny <denny.bevilaqua@accenture.com>
2021-04-19 18:54:24 +0100

    All in header

--- src/LogWebServer.h
+++ src/LogWebServer.h
@@ -1,16 +1,47 @@
+#ifndef LogWebServer_h
+#define LogWebServer_h
+
 #include "ESPAsyncWebServer.h"
 
 class LogWebServerClass
 {
-    private:
-        AsyncWebServer *server;
-        void notFound(AsyncWebServerRequest *request);
-        home(AsyncWebServerRequest *request);
-    
-    public:
-        void begin(AsyncWebServer *s);
-        void log(String msg);
-        void loop();
+private:
+    AsyncWebServer *server;
+
+    void notFound(AsyncWebServerRequest *request)
+    {
+        request->send(404, "text/plain", "Not found");
+    }
+
+    void home(AsyncWebServerRequest *request)
+    {
+        request->send(200, "application/json", "{\"id\": \"12345\", \"hardware\": \"ESP8266\"}");
+    }
+
+public:
+    void begin(AsyncWebServer *s)
+    {
+        server = s;
+
+        server->on("/", HTTP_GET, [&](AsyncWebServerRequest *request){
+                this->home(request);
+            });
+
+        server->onNotFound([&](AsyncWebServerRequest *request){
+                this->notFound(request);
+            });
+    }
+
+    void log(String msg)
+    {
+        
+    }
+
+    void loop()
+    {
+
+    }
 };
 
 LogWebServerClass LogWebServer;
+#endif
